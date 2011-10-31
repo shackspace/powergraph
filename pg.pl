01ceaeae772225c031806d2248ecdba190d9c4c5
hadez <hadez@nrrd.de>
2011-10-31 12:36:18 +0100

    added zoom view and optimized width for netbooks

--- pg.pl
+++ pg.pl
@@ -1,9 +1,18 @@
 #!/usr/bin/perl -W
+#
+#  
+#  1699W [                         :::::::::::|:::::::::::::::::::::::             ]
+#                    ^
+#    ^-- total power consumption in watt  
+
 use strict;
 
 my $CMD='wget -qO - http://powerraw.shack:11111/';
-my $MAX = 10000;
-my $MAXCOLS = 80;
+my $MAX = 35000;
+my $MAXCOLS = 100;
+#my $MAXCOLS = 80 - 7 - 3;
+my $MC2 = $MAXCOLS / 2;
+
 
 while(1) {
   open(FH, "-|", "$CMD")||die("cannot read\n");
@@ -15,16 +24,22 @@ while(1) {
   }
   close(FH);
 
+  printf("%5dW ", $usage);
+
   if($usage > 0)
   {
-    my $ratio = $usage / $MAX;
-    $ratio = 1 if($ratio > 1);
-    my $cols = int($MAXCOLS * $ratio);
-    print '=' x $cols;
+    my $totalratio = $usage / $MAX;
+    $totalratio = 1 if($totalratio > 1);
+
+    my $usagePerTick = $MAX / $MC2;
+    my $zoomratio = ($usage % $usagePerTick) / $usagePerTick;
+
+    printf("[%${MC2}s|%-${MC2}s]", ':' x ($totalratio * $MC2), ':' x ($zoomratio*$MC2));
+
   } else {
     print "|";
   }
+  print $/;
 
-  print " $usage$/";
-  sleep(1);
+  sleep(2);
 }
