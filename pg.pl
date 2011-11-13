c0bff5d28737902c4c6bac4a000b1ba5e1a7c77c
hadez <hadez@nrrd.de>
2011-11-13 21:37:30 +0100

    added ansi color support

--- pg.pl
+++ pg.pl
@@ -15,6 +15,44 @@ my $MAXCOLS = 100;
 my $MC2 = $MAXCOLS / 2;
 
 
+my $aesc = "\x1b";
+my $arst = "${aesc}[0m";
+my $abright = "${aesc}[1m";
+my $abg  = "${aesc}[42m";
+my $aby  = "${aesc}[43m";
+my $abr  = "${aesc}[41m";
+my $afg  = "${aesc}[32m";
+my $afy  = "${aesc}[33m";
+my $afr  = "${aesc}[31m";
+my $aflip ="${aesc}[7m";
+
+my $agn = "$abright$abg$afg";
+my $ayn = "$abright$aby$afy";
+my $arn = "$abright$abr$afr";
+my $agb = "$abright$abg$afg$aflip";
+my $ayb = "$abright$aby$afy$aflip";
+my $arb = "$abright$abr$afr$aflip";
+
+
+
+sub getPowerColor()
+{
+  my @powerlevels = (2000, 3000, 4000, 5000, 6000, 7000);
+  my @powercolors = ($agn, $agb, $ayn, $ayb, $arn, $arb);
+  
+  my $power = shift;
+  for(my $i=0; $i <= $#powerlevels; ++$i)
+  {
+    if($power <= $powerlevels[$i])
+    {
+      return $powercolors[$i];
+    }
+  }
+
+  return $powercolors[$#powercolors];
+}
+
+
 while(1) {
   open(FH, "-|", "$CMD")||die("cannot read\n");
   my $usage = 0;
@@ -35,7 +73,16 @@ while(1) {
     my $usagePerTick = $MAX / $MC2;
     my $zoomratio = ($usage % $usagePerTick) / $usagePerTick;
 
-    printf("[%${MC2}s|%-${MC2}s]", ':' x ($totalratio * $MC2), ':' x ($zoomratio*$MC2));
+    my $col = &getPowerColor($usage);
+
+    my $ntotal =  int ($totalratio * $MC2);
+    my $nzoom = int ($zoomratio * $MC2);
+
+    printf("${arst}[%s${col}%s${arst}|${col}%s${arst}%s]"
+      , ' ' x ($MC2 - $ntotal)
+      , '_' x $ntotal
+      , '_' x $nzoom
+      , ' ' x ($MC2 - $nzoom) );
 
   } else {
     print "|";
